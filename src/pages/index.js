ea162230d5c9dcd4b60a125e566d422579f07654
Erik Oja <erik_oja@yahoo.com>
2022-02-16 13:18:07 -0600

    add Layout, Intro, temp div

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
 import React from "react"
+import Intro from "../components/intro"
+import Layout from "../components/layout"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  
+  
+  return (
+    <Layout>
+      <Intro />
+      <div style={{height: "100vh", backgroundColor: "gray"}}>Temporary</div>
+
+    </Layout>
+  )
 }
