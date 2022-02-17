57b5c983106542472b643359460efc1dfee8b64a
Erik Oja <erik_oja@yahoo.com>
2022-02-17 16:22:17 -0600

    add font awesome

--- src/components/layout.js
+++ src/components/layout.js
@@ -21,6 +21,12 @@ const Layout = ({ children }) => {
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
           crossorigin="anonymous"
         />
+        <link 
+          href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" 
+          rel="stylesheet" 
+          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" 
+          crossorigin="anonymous"
+        />
       </Helmet>
 
       <div>{children}</div>
