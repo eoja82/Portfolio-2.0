2db7e6f12a20af1ea50431b4c5f4a5cefada7335
Erik Oja <erik_oja@yahoo.com>
2022-02-18 15:58:44 -0600

    add Projects

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Intro from "../components/intro"
 import Layout from "../components/layout"
+import Projects from "../components/projects"
 
 export default function Home() {
   
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <Layout>
       <Intro />
+      <Projects />
       <div style={{height: "100vh", backgroundColor: "gray"}}>Temporary</div>
 
     </Layout>
