b86b58032d0255290fbad674121df220db0d2e0c
Erik Oja <erik_oja@yahoo.com>
2022-02-24 13:21:53 -0600

    add temporary meta and icon

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,10 +11,16 @@ const Layout = ({ children }) => {
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        <title></title>
+        {/* add link below to img */}
+        <meta property="og:image" content="" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="400" />
+        <meta property="og:image:height" content="268"></meta>
+        <title>Erik Oja - Web Developer Portfolio</title>
         <meta name="description" content="" />
         <meta name="keywords" content="" />
-        <link rel="icon" type="image/png" href="" />
+        {/* change favicon icon */}
+        <link rel="icon" type="image/png" href="favicon.ico" />
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
