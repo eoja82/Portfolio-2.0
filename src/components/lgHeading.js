94f8aff78f2511c6c03316d131b5b09ed7861843
Erik Oja <erik_oja@yahoo.com>
2022-03-23 13:39:01 -0500

    delete log

--- src/components/lgHeading.js
+++ src/components/lgHeading.js
@@ -3,7 +3,6 @@ import * as styles from "./styles/lgHeading.module.css"
 
 
 const LargeHeading = (props) => {
-  console.log(props)
   return (
     <div>
       <h2 className={styles.header}>{props.title}</h2>
