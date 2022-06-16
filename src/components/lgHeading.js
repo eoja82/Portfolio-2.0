c351d80c3d5a946aa428af48b9867b4912f74fa4
Erik Oja <erik_oja@yahoo.com>
2022-06-16 13:57:07 -0500

    add style attribute

--- src/components/lgHeading.js
+++ src/components/lgHeading.js
@@ -5,7 +5,7 @@ import * as styles from "./styles/lgHeading.module.css"
 const LargeHeading = (props) => {
   return (
     <div>
-      <h2 className={styles.header}>{props.title}</h2>
+      <h2 className={styles.header} style={{textAlign: props.textAlign}}>{props.title}</h2>
     </div>
   )
 }
