a6ab80bc793239f486dc251d7a9ba109c0cee750
Erik Oja <erik_oja@yahoo.com>
2022-06-16 13:56:16 -0500

    add Development component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import Intro from "../components/intro"
 import Layout from "../components/layout"
 import Projects from "../components/projects"
+import Development from "../components/development"
 
 export default function Home() {
   
@@ -10,6 +11,7 @@ export default function Home() {
     <Layout>
       <Intro />
       <Projects />
+      <Development />
       <div style={{height: "100vh", backgroundColor: "gray"}}>Temporary</div>
       {/* About section, color icons for front end
           back end, database, mobile?, testing and other */}
