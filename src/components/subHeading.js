c2aa6da45307041efd9c3cce53aa0da114ae4e92
Erik Oja <erik_oja@yahoo.com>
2022-06-20 13:41:27 -0500

    initial commit of SubHeading component

---
+++ src/components/subHeading.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useRef } from "react"
+import { gsap, ScrollTrigger } from "gsap/all"
+//import PropTypes from "prop-types"
+import * as styles from "./styles/subHeading.module.css"
+
+
+if (typeof window !== undefined) {
+  gsap.registerPlugin(ScrollTrigger)
+}
+
+const SubHeading = (props) => {
+  const title = useRef(null),
+        titleScroller = useRef(null)
+
+  useEffect(() => {
+    gsap.to(titleScroller.current, {
+      y: "-100px",
+      opacity: 1,
+      scrollTrigger: {
+        trigger: titleScroller.current,
+        start: "top bottom",
+        scrub: true,
+        markers: true
+      }
+    })
+  })
+
+  return (
+    <div 
+      ref={titleScroller} 
+      className={styles.titleScroller} 
+      style={{
+        justifyContent: props.justifyContent, 
+        flexDirection: props.flexDirection
+      }}>
+      <div 
+        className={styles.line} 
+        style={{
+          marginLeft: props.marginLeft, 
+          marginRight: props.marginRight
+        }}></div>
+      <h3 className={styles.title} ref={title}>{props.text}</h3>
+    </div>
+  )
+}
+
+export default SubHeading
