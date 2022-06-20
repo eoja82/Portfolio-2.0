95f12c1b7b4e55082a5f720cab335a49773a1d93
Erik Oja <erik_oja@yahoo.com>
2022-06-20 13:42:12 -0500

    use SubHeading component

--- src/components/development.js
+++ src/components/development.js
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from "react"
 import * as styles from "./styles/development.module.css"
 import { gsap, ScrollTrigger } from "gsap/all"
 import LargeHeading from "./lgHeading"
+import SubHeading from "./subHeading"
 import Container from "react-bootstrap/Container"
 
 
@@ -10,33 +11,17 @@ if (typeof window !== undefined) {
 }
 
 const Development = () => {
-  const titleContainer= useRef(null),
-        titleScroller = useRef(null),
-        title = useRef(null)
-
-  useEffect(() => {
-    
-    gsap.to(titleScroller.current, {
-      y: "-100px",
-      opacity: 1,
-      scrollTrigger: {
-        trigger: titleScroller.current,
-        start: "top bottom",
-        scrub: true,
-        markers: true
-      }
-    })
-  })
-
   return (
     <div>
       <Container fluid="true">
-        <div className={styles.titleContainer} ref={titleContainer}>
+        <div className={styles.titleContainer}>
           <LargeHeading title="Development" textAlign="right" />
-          <div ref={titleScroller} className={styles.titleScroller}>
-            <h3 className={styles.title} ref={title}>Front-End</h3>
-            <div className={styles.lineAfter}></div>
-          </div>
+          <SubHeading 
+            text="Front End" 
+            justifyContent="end" 
+            flexDirection="row-reverse"
+            marginLeft="2rem" 
+          />
         </div>
       </Container>
     </div>
