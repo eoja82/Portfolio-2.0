fdb1e2dd80695256c13f4166da4f67101f24501d
Erik Oja <erik_oja@yahoo.com>
2022-08-16 17:10:51 -0500

    add Topnav

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,16 @@
 import React from "react"
 import Contact from "../components/contact"
+import Development from "../components/development"
 import Intro from "../components/intro"
 import Layout from "../components/layout"
 import Projects from "../components/projects"
-import Development from "../components/development"
+import Topnav from "../components/topnav"
+
 
 export default function Home() {  
   return (
     <Layout>
+      <Topnav />
       <Intro />
       <Projects />
       <Development />
