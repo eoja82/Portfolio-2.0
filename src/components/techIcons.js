91698b036dc1ccbebb8a4534199b57ced3a794c6
Erik Oja <erik_oja@yahoo.com>
2022-08-25 15:54:59 -0500

    restyled with bs Card

--- src/components/techIcons.js
+++ src/components/techIcons.js
@@ -1,5 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
+import Card from "react-bootstrap/Card"
 import Col from "react-bootstrap/Col"
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
@@ -8,23 +9,28 @@ import * as styles from "./styles/techIcons.module.css"
 
 const TechIcons = (props) => {
   return (
-    <Container fluid className={styles.container}>
-      <Container>
-      <Row className={styles.row}>
+    <Container className={styles.container}>
+      <Row className={styles.row + " g-4"}>
         {props.data.map( x => (
-          <Col xs={6} sm={4} md={3} lg={2} xl={2} className={styles.col} key={x.title}>
-            <div className={styles.wrapper} style={{borderBottom: `5px solid ${x.borderColor}`}}>
-              <div className={styles.iconContainer}>
-                <div className={styles.iconWrapper}>
-                  <img className={styles.icon} src={x.src} alt={x.title}></img>
-                </div>
-              </div>
-              <p className={styles.title + " text-muted text-center"}>{x.title}</p>
-            </div>
+          <Col xs={4} sm={3} md={3} lg={2} xl={2} xxl={2} className={styles.col} key={x.title}>
+            <Card style={{
+              borderBottom: `5px solid ${x.borderColor}`,
+              borderTop: "none",
+              borderRight: "none",
+              borderLeft: "none",
+              borderRadius: "0",
+              boxShadow: "-2px 8px 19.95px 1.05px rgba(156, 156, 156, 0.24)",
+              wordWrap: "normal"}}>
+              <Card.Body className={styles.iconWrapper}>
+                <Card.Img className={styles.icon} src={x.src} alt={x.title} style={{width: "auto"}} />
+              </Card.Body>
+              <Card.Body>
+                <Card.Text className="text-muted text-center">{x.title}</Card.Text>
+              </Card.Body>
+            </Card>
           </Col>
         ))}
       </Row>
-      </Container>
     </Container>
   )
 }
