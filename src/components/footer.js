973bd0f33c84d126d8de98cb8ce6273b46743e3c
Erik Oja <erik_oja@yahoo.com>
2022-09-23 18:42:55 -0500

    add footer component

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Container from "react-bootstrap/Container"
+
+const Footer = () => {
+  return (
+    <Container fluid="true" className="mt-3" as="footer">
+      <p className="text-center fw-light text-light">&copy; 2022 Erik Oja.  All rights reserved.</p>
+    </Container>
+  )
+}
+
+export default Footer
