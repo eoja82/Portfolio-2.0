2ee9e5c5bdb85c03804e87485076e50355860087
Erik Oja <erik_oja@yahoo.com>
2022-09-27 18:30:48 -0500

    delete navigation

--- src/components/intro.js
+++ src/components/intro.js
@@ -3,7 +3,6 @@ import * as styles from "./styles/intro.module.css"
 import { gsap } from "gsap"
 import { ScrollTrigger } from "gsap/all"
 import Container from "react-bootstrap/Container"
-import Nav from "react-bootstrap/Nav"
 
 if (typeof window !== undefined) {
   gsap.registerPlugin(ScrollTrigger)
@@ -15,8 +14,7 @@ const Intro = () => {
         heroSubTitle = useRef(null),
         angle = useRef(null),
         topBorder = useRef(null),
-        bottomBorder = useRef(null),
-        nav = useRef(null)
+        bottomBorder = useRef(null)
   
   // on scroll animations
   useLayoutEffect(() => {
@@ -54,11 +52,6 @@ const Intro = () => {
       duration: 1.25, 
       delay: 2
     })
-    gsap.from(nav.current, {
-      opacity: 0,
-      delay: 3.25,
-      duration: 1
-    })
     gsap.from(angle.current, {
       opacity: 0,
       delay: 3.25,
@@ -70,18 +63,6 @@ const Intro = () => {
 
   return (
     <div>
-      <style>
-        {`
-          .navLink {
-            color: white;
-          }
-          .navLink:hover,
-          .navLink:focus {
-            color: #221f1f;
-            background-color: white;
-          }
-        `}
-      </style>
       <Container ref={introSection} id="intro" fluid="true" className={styles.heroContainer}>
         <div className={styles.titleContainer}>
           <div className={styles.topBorder} ref={topBorder}></div>
@@ -91,17 +72,6 @@ const Intro = () => {
           </div>
           <div className={styles.bottomBorder} ref={bottomBorder}></div>
         </div>
-        <Nav ref={nav} className={styles.navLinks + " justify-content-center"}>
-          <Nav.Item>
-            <Nav.Link href="#projects" className={styles.navLink1 + " navLink"}>Projects</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="#development" className={styles.navLink2 + " navLink"}>Development</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="#contact" className={styles.navLink3 + " navLink"}>Contact</Nav.Link>
-          </Nav.Item>
-        </Nav>
         <i className={styles.angle + " fa fa-angle-down"} ref={angle} style={{fontSize: "2rem"}}></i>
       </Container>
     </div>
