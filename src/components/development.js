7ab299d407f976c5dc2f03f2b21328dd10b5bad6
Erik Oja <erik_oja@yahoo.com>
2022-09-30 17:28:36 -0500

    Revert "delete div from around Container"
    This reverts commit 98a53e23ba4802aa53c436e7eaaca6bf92ca213c.

--- src/components/development.js
+++ src/components/development.js
@@ -6,18 +6,20 @@ import Container from "react-bootstrap/Container"
 
 const Development = () => {
   return (
-    <Container id="development" className={styles.developmentContainer}>
-      <h1 className={styles.header}>Front End</h1>
-      <TechIcons data={frontEnd} />
-      <h1 className={styles.header}>Back End</h1>
-      <TechIcons data={backEnd} />
-      <h1 className={styles.header}>Database</h1>
-      <TechIcons data={database} />
-      <h1 className={styles.header}>Testing</h1>
-      <TechIcons data={testing} />
-      <h1 className={styles.header}>Version Control</h1>
-      <TechIcons data={versionControl} />
-    </Container>
+    <div>
+      <Container id="development" className={styles.developmentContainer}>
+        <h1 className={styles.header}>Front End</h1>
+        <TechIcons data={frontEnd} />
+        <h1 className={styles.header}>Back End</h1>
+        <TechIcons data={backEnd} />
+        <h1 className={styles.header}>Database</h1>
+        <TechIcons data={database} />
+        <h1 className={styles.header}>Testing</h1>
+        <TechIcons data={testing} />
+        <h1 className={styles.header}>Version Control</h1>
+        <TechIcons data={versionControl} />
+      </Container>
+    </div>
   )
 }
 
