98a53e23ba4802aa53c436e7eaaca6bf92ca213c
Erik Oja <erik_oja@yahoo.com>
2022-09-30 16:45:20 -0500

    delete div from around Container

--- src/components/development.js
+++ src/components/development.js
@@ -6,20 +6,18 @@ import Container from "react-bootstrap/Container"
 
 const Development = () => {
   return (
-    <div>
-      <Container id="development" className={styles.developmentContainer}>
-        <h1 className={styles.header}>Front End</h1>
-        <TechIcons data={frontEnd} />
-        <h1 className={styles.header}>Back End</h1>
-        <TechIcons data={backEnd} />
-        <h1 className={styles.header}>Database</h1>
-        <TechIcons data={database} />
-        <h1 className={styles.header}>Testing</h1>
-        <TechIcons data={testing} />
-        <h1 className={styles.header}>Version Control</h1>
-        <TechIcons data={versionControl} />
-      </Container>
-    </div>
+    <Container id="development" className={styles.developmentContainer}>
+      <h1 className={styles.header}>Front End</h1>
+      <TechIcons data={frontEnd} />
+      <h1 className={styles.header}>Back End</h1>
+      <TechIcons data={backEnd} />
+      <h1 className={styles.header}>Database</h1>
+      <TechIcons data={database} />
+      <h1 className={styles.header}>Testing</h1>
+      <TechIcons data={testing} />
+      <h1 className={styles.header}>Version Control</h1>
+      <TechIcons data={versionControl} />
+    </Container>
   )
 }
 
