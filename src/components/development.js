73187b390fe181e73273c9d61327aed1fa611b28
Erik Oja <erik_oja@yahoo.com>
2022-09-30 16:36:10 -0500

    restyle for tab layout

--- src/components/development.js
+++ src/components/development.js
@@ -1,79 +1,26 @@
-import React, { forwardRef } from "react"
+import React from "react"
 import * as styles from "./styles/development.module.css"
-import LargeHeading from "./lgHeading"
-import SubHeading from "./subHeading"
 import TechIcons from "./techIcons"
 import { backEnd, database, frontEnd, testing, versionControl } from "./data/data.js"
 import Container from "react-bootstrap/Container"
 
-const Development = forwardRef((props, developmentSection) => {
+const Development = () => {
   return (
     <div>
-      <Container id="development" ref={developmentSection} className={styles.developmentContainer} fluid="true">
-        <LargeHeading title="Development" textAlign="right" />
-        <div id="frontEnd">
-          <div className={styles.titleContainer}>
-            <SubHeading 
-              text="Front End" 
-              justifyContent="end" 
-              flexDirection="row-reverse"
-              marginLeft="2rem"
-              color="#ff5722"
-            />
-          </div>
-          <TechIcons data={frontEnd} />
-        </div>
-        <div id="backEnd">
-          <div className={styles.titleContainer}>
-            <SubHeading 
-              text="Back End"
-              justifyContent="start"
-              flexDirection="row"
-              marginRight="2rem"
-              color="#549e44"
-            />
-          </div>
-          <TechIcons data={backEnd} />
-        </div>
-        <div id="database">
-          <div className={styles.titleContainer}>
-            <SubHeading 
-              text="Database"
-              justifyContent="end"
-              flexDirection="row-reverse"
-              marginLeft="2rem"
-              color="#ffd54f"
-            />
-          </div>
-          <TechIcons data={database} />
-        </div>
-        <div id="testing">
-          <div className={styles.titleContainer}>
-            <SubHeading 
-              text="Testing"
-              justifyContent="start"
-              flexDirection="row"
-              marginRight="2rem"
-              color="#8d6748"
-            />
-          </div>
-          <TechIcons data={testing} />
-        </div>
-        <div id="versionControl">
-          <div className={styles.titleContainer}>
-            <SubHeading 
-              text="Version Control"
-              justifyContent="end"
-              flexDirection="row-reverse"
-              marginLeft="2rem"
-              color="#de4c36"
-            />
-          </div>
-          <TechIcons data={versionControl} />
-        </div>
+      <Container id="development" className={styles.developmentContainer}>
+        <h1 className={styles.header}>Front End</h1>
+        <TechIcons data={frontEnd} />
+        <h1 className={styles.header}>Back End</h1>
+        <TechIcons data={backEnd} />
+        <h1 className={styles.header}>Database</h1>
+        <TechIcons data={database} />
+        <h1 className={styles.header}>Testing</h1>
+        <TechIcons data={testing} />
+        <h1 className={styles.header}>Version Control</h1>
+        <TechIcons data={versionControl} />
       </Container>
     </div>
   )
-})
+}
 
 export default Development
