c7e0019338c5196f8959c91b8421a56c8da0e2ca
Erik Oja <erik_oja@yahoo.com>
2022-09-30 16:35:29 -0500

    restyle component

--- src/components/techIcons.js
+++ src/components/techIcons.js
@@ -1,36 +1,20 @@
 import React from "react"
 import PropTypes from "prop-types"
-import Card from "react-bootstrap/Card"
-import Col from "react-bootstrap/Col"
 import Container from "react-bootstrap/Container"
-import Row from "react-bootstrap/Row"
 import * as styles from "./styles/techIcons.module.css"
 
 
 const TechIcons = (props) => {
   return (
     <Container className={styles.container}>
-      <Row className={styles.row + " g-4"}>
+      <ul className={styles.uList}>
         {props.data.map( x => (
-          <Col xs={4} sm={3} md={3} lg={2} xl={2} xxl={2} key={x.title}>
-            <Card style={{
-              borderBottom: `5px solid ${x.borderColor}`,
-              borderTop: "none",
-              borderRight: "none",
-              borderLeft: "none",
-              borderRadius: "0",
-              boxShadow: "-2px 8px 19.95px 1.05px rgba(156, 156, 156, 0.24)",
-              wordWrap: "normal"}}>
-              <Card.Body className={styles.iconWrapper}>
-                <Card.Img className={styles.icon} src={x.src} alt={x.title} style={{width: "auto"}} />
-              </Card.Body>
-              <Card.Body>
-                <Card.Text className="text-muted text-center">{x.title}</Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
+          <li className={styles.li + " text-light"}>
+          <img src={x.src} alt={`${x.title} icon`} className={styles.img}></img>
+          {x.title}
+        </li>
         ))}
-      </Row>
+      </ul>
     </Container>
   )
 }
