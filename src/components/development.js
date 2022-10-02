0afd377917bc655d7991905dec7667908d5535a1
Erik Oja <erik_oja@yahoo.com>
2022-10-02 17:03:59 -0500

    add TabHeading

--- src/components/development.js
+++ src/components/development.js
@@ -1,13 +1,15 @@
 import React from "react"
 import * as styles from "./styles/development.module.css"
+import TabHeading from "./tabHeading"
 import TechIcons from "./techIcons"
 import { backEnd, database, frontEnd, testing, versionControl } from "./data/data.js"
 import Container from "react-bootstrap/Container"
 
 const Development = () => {
   return (
-    <div>
-      <Container id="development" className={styles.developmentContainer}>
+    <Container fluid="true" className={styles.developmentContainer}>
+      <TabHeading text="Development Tools" />
+      <Container id="development" className={styles.development}>
         <h1 className={styles.header}>Front End</h1>
         <TechIcons data={frontEnd} />
         <h1 className={styles.header}>Back End</h1>
@@ -19,7 +21,7 @@ const Development = () => {
         <h1 className={styles.header}>Version Control</h1>
         <TechIcons data={versionControl} />
       </Container>
-    </div>
+    </Container>
   )
 }
 
