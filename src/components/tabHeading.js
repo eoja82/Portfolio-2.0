719d4b853519cb017ed81d9328224fb9845ffd4f
Erik Oja <erik_oja@yahoo.com>
2022-10-03 13:29:14 -0500

    change img src

--- src/components/tabHeading.js
+++ src/components/tabHeading.js
@@ -9,7 +9,7 @@ const TabHeading = (props) => {
   return (
     <Container className={styles.container} fluid="true">
       <Container className={styles.imgContainer}>
-        <Image src="./img/laptop2copy.jpg" alt="picture of a laptop" className={styles.img}  roundedCircle></Image>
+        <Image src="./img/laptop.jpg" alt="picture of a laptop" className={styles.img}  roundedCircle></Image>
       </Container>
       <Container className={styles.textContainer}>
         <h1 className={styles.header}>{props.text}</h1>
