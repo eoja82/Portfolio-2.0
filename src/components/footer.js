b7cf6a1a5a60d3dc7f84016b460aec9ed4e2522a
Erik Oja <erik_oja@yahoo.com>
2022-10-10 12:42:44 -0500

    add position and bg color

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,10 @@
 import React from "react"
 import Container from "react-bootstrap/Container"
+import * as styles from "../components/styles/footer.module.css"
 
 const Footer = () => {
   return (
-    <Container fluid="true" className="mt-3" as="footer">
+    <Container fluid="true" as="footer" className={styles.footerContainer }>
       <p className="text-center fw-light text-light">&copy; 2022 Erik Oja.  All rights reserved.</p>
     </Container>
   )
