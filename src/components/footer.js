c2c759d435835436b773f39f97bf5c81ca004e40
Erik Oja <erik_oja@yahoo.com>
2022-10-13 14:27:56 -0500

    add style prop

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,18 @@
 import React from "react"
 import Container from "react-bootstrap/Container"
+import PropTypes from "prop-types"
 import * as styles from "../components/styles/footer.module.css"
 
-const Footer = () => {
+const Footer = (props) => {
   return (
-    <Container fluid="true" as="footer" className={styles.footerContainer }>
+    <Container fluid="true" as="footer" className={styles.footerContainer} style={props.style}>
       <p className="text-center fw-light text-light">&copy; 2022 Erik Oja.  All rights reserved.</p>
     </Container>
   )
+}
+
+Footer.prototypes = {
+  style: PropTypes.object
 }
 
 export default Footer
