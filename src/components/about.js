604f546f638c8527d2087f348b72422c5af3c82d
Erik Oja <erik_oja@yahoo.com>
2022-10-17 13:40:18 -0500

    fix max-width for text

--- src/components/about.js
+++ src/components/about.js
@@ -9,16 +9,18 @@ const About = () => {
   return (
     <Container fluid="true" className={styles.aboutContainer} id="about">
       <TabHeading text="About" />
-      <Container className={styles.about}>
-        <p>
-          I love to code, solve problems, and "make things work."
-        </p>
-        <p>
-          For the past few years I have been developing web applications using HTML, CSS, JavaScript and modern frameworks and technologies like React, Gatsby and Bootstrap among others. For applications requiring a back end solution I have been utilizing Node / Express or Python / Django.
-        </p>
-        <p>
-          I enjoy learning new languages and technologies and I'm always looking to learn something new to level up my skills or provide a better user experience for applications.
-        </p>
+      <Container>
+        <div className={styles.about}>
+          <p>
+            I love to code, solve problems, and "make things work."
+          </p>
+          <p>
+            For the past few years I have been developing web applications using HTML, CSS, JavaScript and modern frameworks and technologies like React, Gatsby and Bootstrap among others. For applications requiring a back end solution I have been utilizing Node / Express or Python / Django.
+          </p>
+          <p>
+            I enjoy learning new languages and technologies and I'm always looking to learn something new to level up my skills or provide a better user experience for applications.
+          </p>
+        </div>
       </Container>
       <Footer />
     </Container>
