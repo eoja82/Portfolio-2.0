2060b3847e73e9ed581f9926e21151dbfa407a7b
Erik Oja <erik_oja@yahoo.com>
2022-10-18 13:42:34 -0500

    add meta content

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,15 +12,13 @@ const Layout = ({ children }) => {
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        {/* add link below to img */}
-        <meta property="og:image" content="" />
+        <meta property="og:image" content="https://www.erikoja.com/img/projects/portfolio2.png" />
         <meta property="og:image:type" content="image/png" />
         <meta property="og:image:width" content="400" />
         <meta property="og:image:height" content="268"></meta>
         <title>Erik Oja - Web Developer Portfolio</title>
-        <meta name="description" content="" />
-        <meta name="keywords" content="" />
-        {/* change favicon icon */}
+        <meta name="description" content="I'm a full stack developer that loves to learn.  With experience in a variety of languages, technologies, and databases my mission is to build intuitive and responsive web applications.  View some of my projects." />
+        <meta name="keywords" content="Erik Oja, web, developer, software, JavaScript, React, Gatsby, HTML, CSS, Python, Django, Bootsrtap, hire, Minnesota, work" />
         <link rel="icon" type="image/png" href="favicon.ico" />
         <link
           rel="stylesheet"
